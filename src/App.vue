785f23c961a5d86e93202b5a5587887a4d1c1d11
Camilo Álvarez <camiloalvarez0521@gmail.com>
2021-05-18 21:41:39 -0500

    Welcome view completed

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,4 @@
 
 @import 'styles/main.scss';
 
-#app {
-  font-family: 'Lato', sans-serif;
-}
-
-// #nav {
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,16 @@
 <template>
-  <h1>Welcome to Pokédex</h1>
-  <button>Prueba</button>
+  <div class="main-container">
+    <div class="logo">
+      <img src="../assets/img/pokemon-logo.svg" alt="pokemon" />
+    </div>
+    <div class="description">
+      <h1>Welcome to Pokédex</h1>
+      <p>The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.</p>
+    </div>
+    <div class="get-started">
+      <button>Get started</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,7 +18,30 @@ export default {};
 </script>
 
 <style lang="scss">
+@import "../styles/main.scss";
 
-@import '../styles/main.scss';
+.main-container {
+  @include max-width(1200px);
+  @include flex-center;
+  flex-flow: column nowrap;
+  justify-content: space-evenly;
+  height: 100vh;
+  padding: 0 30px;
 
+  .description {
+    text-align: center;
+
+    h1 {
+      margin-bottom: 30px;
+    }
+  }
+
+  .get-started {
+    button {
+      @include orange-button;
+    }
+  }
+
+
+}
 </style>
