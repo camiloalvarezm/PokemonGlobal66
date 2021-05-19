659ebff61e1c302a8fc2584889d371b41d6a3e06
Camilo Álvarez <camiloalvarez0521@gmail.com>
2021-05-18 20:06:36 -0500

    SASS Styles and welcome component added

---
+++ src/views/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome to Pokédex</h1>
+  <button>Prueba</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+
+@import '../styles/main.scss';
+
+</style>
